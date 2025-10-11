8db185ba0a975425485de7128c531e06cc9e7a03
Panos Pitelos <pitepana@gmail.com>
2025-10-11 19:37:10 +0300

    new program

--- helloworld.asm
+++ helloworld.asm
@@ -1,4 +1,13 @@
-        * = $c000
-        lda #$00
-        sta $d020
-        rts
+        BasicUpstart2(start)
+        .encoding "petscii_upper"
+    
+start:  ldx #$00
+loop:   lda htext,x
+        beq done
+        jsr $ffd2
+        inx
+        jmp loop
+done:   rts
+
+htext: .text "HELLO WORLD"
+        .byte 0
