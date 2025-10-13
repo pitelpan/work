b413ac68de93be44cb299a4f286035b862bec474
Panos Pitelos <pitepana@gmail.com>
2025-10-13 17:29:40 +0300

    changes

--- ASM/hello.asm
+++ ASM/hello.asm
@@ -1,41 +1,44 @@
-TD_OUTPUT_HANDLE equ -11
-%define NULL    dword 0
-
-    extern  _GetStdHandle@4
-    extern  _WriteFile@20
-    extern  _ExitProcess@4
-
-    global _main
-
-    section .text
-
-_main:
-    ; local variable
-bytesWritten equ -4
-    mov     ebp, esp
-    sub     esp, 4
-
-    ; hStdOut = GetstdHandle(STD_OUTPUT_HANDLE)
-    push    STD_OUTPUT_HANDLE
-    call    _GetStdHandle@4
-    mov     ebx, eax
-
-    ; WriteFile(hstdOut, message, length(message),
-    ;           &bytesWritten, null);
-    push    NULL
-    lea     eax, [ebp + bytesWritten]
-    push    eax
-    push    (message_end - message)
-    push    message
-    push    ebx
-    call    _WriteFile@20
-
-    ; ExitProcess(0)
-    push    0
-    call    _ExitProcess@4
-
-    ; never here
-    hlt
-message:
-    db      'Hello, World', 10
-message_end:
+; Example 64-bit Assembly Program for MinGW
+; Filename: hello2.asm
+default rel   ; Use RIP-relative addressing by default
+; Data section
+section .data
+    msg db "Hello, World!", 13, 10  ; Message with CR+LF
+    msglen equ $ - msg              ; Length of message
+    STD_OUTPUT_HANDLE equ -11       ; Windows constant for stdout
+    NULL equ 0
+    
+section .bss
+    stdout resq 1                   ; Reserve space for handle
+    written resq 1                  ; Reserve space for bytes written
+; Import necessary Windows functions
+extern GetStdHandle
+extern WriteConsoleA
+extern ExitProcess
+; Code section
+section .text
+    global main
+main:
+    ; Set up stack frame
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 32                ; Shadow space for function calls
+    ; Get handle to stdout
+    mov     ecx, STD_OUTPUT_HANDLE
+    call    GetStdHandle
+    mov     qword [rel stdout], rax
+    ; Write to console
+    mov     rcx, qword [rel stdout] ; Console handle
+    lea     rdx, [rel msg]         ; Message to write
+    mov     r8, msglen             ; Message length
+    lea     r9, [rel written]      ; Bytes written
+    push    NULL                   ; Overlapped
+    sub     rsp, 32                ; Shadow space
+    call    WriteConsoleA
+    add     rsp, 40                ; Clean up stack (32 + 8)
+    ; Exit program
+    xor     ecx, ecx               ; Exit code 0
+    call    ExitProcess
+    ; Function epilogue
+    leave
+    ret
