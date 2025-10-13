29cccb307caadb82b9eeb770ad39386811d8e367
Panos Pitelos <pitepana@gmail.com>
2025-10-13 21:32:55 +0300

    add

---
+++ C/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int video_num=2;
+    char name [] = "PanosPitelos";
+    
+    printf("This is video %d\n",video_num);
+    printf("my youtube is %s\n", name);
+    return 0;
+
+}
