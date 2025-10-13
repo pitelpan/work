e330ed4f9c316afe32012b020a1490d18494c391
Panos Pitelos <pitepana@gmail.com>
2025-10-13 17:10:35 +0300

    modify

--- ASM/hello.asm
+++ ASM/hello.asm
@@ -1,21 +1,20 @@
+dosseg
 .model small
-
 .stack 100h
-
 .data
-
-hello db 10,13,”Hello World$”
-
+msg1 db 'Hello World$'
 .code
+main PROC
+    
+    mov ax,@data
+    mov ds,ax
+    mov dl,msg1
+    mov dx,offset msg1
+    mov ah,9
+    int 21h
+    
+    mov ah,4ch
+    int 21h
 
-main proc
-
-lea dx, hello
-
-mov ah, 9h
-
-int 21h
-
-main endp
-
+main ENDP
 end main
