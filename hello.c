cd3e398008c4207fb6fab03c0b52240b8010bc89
Panos Pitelos <pitepana@gmail.com>
2025-10-15 19:17:11 +0300

    deleted asm code

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const double PI = 3.14159;
+    
+    // Print a simple message to the console
+    char character = 'A';
+    printf ("The character is: %c\n", character);
+    printf("The value of PI is: %f\n", PI);
+
+    return 0;
+}
